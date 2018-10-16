ab255b4130f7b9f5dc16cec9c8b969e705892a5c
Nat Reid <nathanielreid@gmail.com>
2018-10-16 00:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function createRock(x) {
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-    var speed = 6;
+    var speed = 3;
     function step() {
       rock.style.top = `${top += speed}px`
       if (top < GAME_HEIGHT) {

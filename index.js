cd1b420ef25dee2ff1a7d18cc73b40b36d3a9944
Nat Reid <nathanielreid@gmail.com>
2018-10-16 00:19:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ function start() {
       if (gameInterval) {
         repeat();
       }
-    }, Math.max(Math.floor(1200-Math.log10(score+1)*800)) + Math.floor(Math.random()*200));
+    }, Math.max(Math.floor(1200-Math.log10(score+1)*600)) + Math.floor(Math.random()*200));
   }
   repeat();
 

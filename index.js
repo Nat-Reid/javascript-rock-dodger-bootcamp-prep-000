0351b8d54c374265ab26a1c04618831c6a6114a3
Nat Reid <nathanielreid@gmail.com>
2018-10-16 00:51:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function createRock(x) {
       if (top < GAME_HEIGHT) {
         if (checkCollision(rock)){
           //endGame();
-          speed = Math.min(speed * (-1)+ 0.1,-3);
+          speed = Math.min(speed * (-1)+ 0.1,-2);
         }
         speed += 0.12
         window.requestAnimationFrame(step)
